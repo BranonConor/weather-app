de8ef38e53ef95767a5f8140b3e0eec572ab97c0
branonconor <breusebio@westmont.edu>
2020-03-13 18:47:08 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+//initialize express
+const express = require('express');
+const app = express();
+const request = require('request');
+const path = require('path');
+
+//initialize EJS
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+// ----------- ROUTES ------------- //
+// ROOT ROUTE
+app.get('/', (req, res) => {
+    res.render('search');
+});
+
+app.get('/weather', (req, res) => {
+    let query = (req.query.search);
+    request(`http://api.weatherapi.com/v1/current.json?key=7506c717755f4a7ab1502045201403&q=${query}`, (error, response, body) => {   
+        let data = JSON.parse(body);
+        console.log(data); 
+        const weather = {
+            city: data.location.name,
+            state: data.location.region,
+            time: data.location.localtime,
+            icon: data.current.condition.icon,
+            weather: data.current.condition.text,
+            temperature: data.current.temp_f,
+            wind: data.current.wind_mph,
+            direction: data.current.wind_dir,
+            date: data.forecastDay,
+        };
+        res.render('weather', {weather: weather});    
+    });
+});
+
+
+//enable listening for server
+app.listen(8000, process.env.IP, (req, res) => {
+    console.log('Server listening on port 8000');
+});
