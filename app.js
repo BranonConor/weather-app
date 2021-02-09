4c936cef40fc588a2a2b30a85e9985831d18b5da
branonconor <breusebio@westmont.edu>
2021-02-08 23:01:24 -0800

    Restyling the UI to try Neumorphism and cleaned up API calls in backend

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //initialize express
 const express = require('express');
 const app = express();
-const request = require('request');
 const path = require('path');
+const fetch = require('node-fetch');
 
 //initialize EJS
 app.set('view engine', 'ejs');
@@ -13,11 +13,13 @@ app.get('/', (req, res) => {
     res.render('search');
 });
 
-app.get('/weather', (req, res) => {
-    let query = (req.query.search);
-    request(`http://api.weatherapi.com/v1/current.json?key=7506c717755f4a7ab1502045201403&q=${query}`, (error, response, body) => {   
-        let data = JSON.parse(body);
-        console.log(data); 
+app.get('/weather', async (req, res) => {
+    try {
+        //make API calls
+        let query = (req.query.search);
+        let data = await fetch(`http://api.weatherapi.com/v1/current.json?key=7506c717755f4a7ab1502045201403&q=${query}`)
+            .then(res => res.json());
+        //breakdown the values to be passed into templates
         const weather = {
             city: data.location.name,
             region: data.location.region,
@@ -30,10 +32,12 @@ app.get('/weather', (req, res) => {
             direction: data.current.wind_dir,
             date: data.forecastDay,
         };
-        res.render('weather', {weather: weather});    
-    });
+        //render the weather page with the data
+        res.render('weather', {weather: weather});
+    } catch (error) {
+        console.log(error);
+    }
 });
-
 
 //enable listening for server
 app.listen(process.env.PORT || 4000, process.env.IP, (req, res) => {
